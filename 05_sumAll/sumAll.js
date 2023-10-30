e4ce944a95795a8a39a3c3d1936a02de0357307f
Tudor Jianu <jianu.tudor9@gmail.com>
2023-10-30 07:50:20 +0000

    update exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(lowInt, highInt) {
+  switch (true) {
+    case lowInt < 0 || highInt < 0:
+      return "ERROR";
+    case typeof lowInt !== 'number' || typeof highInt !== 'number':
+      return "ERROR";
+    case highInt < lowInt:
+      [lowInt, highInt] = [highInt, lowInt];
+      break;
+    default:
+      break;
+  }
+  let sum = 0;
+  for (i = lowInt; i <= highInt; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahr) {
+  // C = (F - 32) * 5/9
+  cels = (fahr - 32) * 5 / 9
+  return Math.round(cels * 10) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cels) {
+  // F = C * 9/5 + 32
+  fahr = (cels * 9 / 5) + 32
+  return Math.round(fahr * 10) / 10
 };
 
 // Do not edit below this line
