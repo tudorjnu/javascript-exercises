c79557ff0f4965ac74881bb5af6ccdf5e0cb90a3
Tudor Jianu <jianu.tudor9@gmail.com>
2023-10-30 07:49:45 +0000

    update exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(ls) {
+  result = 0;
+  ls.forEach((element) => {
+    result += element;
+  });
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(ls) {
+  result = 1;
+  ls.forEach((element) => {
+    result *= element;
+  });
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  result = a;
+  for (i = 1; i < b; i++) {
+    result *= a;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  result = 1;
+  for (i = 1; i < a + 1; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
