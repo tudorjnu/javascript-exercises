b59e7b54cf4fd2365ef1bbcb2f87b97eb5b186da
Tudor Jianu <jianu.tudor9@gmail.com>
2023-10-31 07:37:54 +0000

    solved

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,26 @@
-const palindromes = function () {
+function removeNonAlphabet(str) {
+  const validChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let result = '';
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    if (validChars.includes(char)) {
+      result += char;
+    }
+  }
+  return result;
+}
 
+const reverse = str => str.split('').reverse().join('');
+
+
+
+
+const palindromes = function(str) {
+  str = str.trim();
+  str = str.toLowerCase();
+  str = removeNonAlphabet(str)
+  rev = reverse(str);
+  return rev === str;
 };
 
 // Do not edit below this line
