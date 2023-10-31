116774279d58eeec91eb4160fb385350384399d9
Tudor Jianu <jianu.tudor9@gmail.com>
2023-10-31 08:09:59 +0000

    solve

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+  if (n < 0) {
+    return "OOPS";
+  }
+  if (typeof n === String) {
+    n = parseInt(n);
+  }
 
+  function fibonacci(n0, n1, n) {
+    if (n == 1) {
+      return n0;
+    } else {
+      return fibonacci(n1, n0 + n1, n - 1);
+    }
+  }
+  return fibonacci(1, 1, n)
 };
+
+console.log(fibonacci(4));
 
 // Do not edit below this line
 module.exports = fibonacci;
