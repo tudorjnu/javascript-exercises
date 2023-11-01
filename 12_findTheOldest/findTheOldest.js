fedb2596059090df4360a58c8f1f14dc31f3ea9d
Tudor Jianu <jianu.tudor9@gmail.com>
2023-11-01 07:47:27 +0000

    solve findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,29 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  let oldestPerson = null;
+  let oldestAge = 0;
 
+  for (const person of people) {
+    let age;
+
+    if ("yearOfDeath" in person) {
+      age = person.yearOfDeath - person.yearOfBirth;
+    } else {
+      age = new Date().getFullYear() - person.yearOfBirth;
+    }
+
+    if (oldestPerson === null) {
+      oldestPerson = person;
+      oldestAge = age;
+      continue;
+    } else if (oldestAge > age) {
+      continue;
+    }
+    oldestPerson = person;
+    oldestAge = age;
+  }
+  return oldestPerson;
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
